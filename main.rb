fe4e61dddaf32944e9220ab7e56c4d2435a2e90a
stse416 <43531621+stse416@users.noreply.github.com>
2025-11-29 20:58:25 -0500

    First commit - MVP

---
+++ main.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# puts "   | x | o "
+# puts "-----------"
+# puts "   | x |   "
+# puts "-----------"
+# puts " x | o |   "
+
+class TTT_Game
+  attr_reader :board
+
+  def initialize
+    @board = Array.new(9, "   ")
+    @turn_num = 0
+    @horiz1 = [0, 1, 2]
+    @horiz2 = [3, 4, 5]
+    @horiz3 = [6, 7, 8]
+    @vert1 = [0, 3, 6]
+    @vert2 = [1, 4, 7]
+    @vert3 = [2, 5, 8]
+    @diag1 = [0, 4, 8]
+    @diag2 = [2, 4, 6]
+    @win_lines = [@horiz1, @horiz2, @horiz3, @vert1, @vert2, @vert3, @diag1, @diag2]
+  end
+
+  def show_board
+    puts "#{board[0]}|#{board[1]}|#{board[2]}"
+    puts "-----------"
+    puts "#{board[3]}|#{board[4]}|#{board[5]}"
+    puts "-----------"
+    puts "#{board[6]}|#{board[7]}|#{board[8]}"
+  end
+
+  def play_move
+    puts "Please enter a integer from 0 to 8, with 0 being top left and 8 being bottom right."
+    num = Integer(gets, exception: false)
+
+    if !num || num < 0 || num > 8 # rubocop:disable Style/NumericPredicate
+      puts "Not a valid integer."
+    elsif @board[num] != "   "
+      puts "Invalid entry, space already occupied"
+    else
+      @board[num] = (@turn_num % 2).even? ? " x " : " o "
+      return if check_win?(num) == true
+
+      @turn_num += 1
+    end
+
+    show_board
+    play_move
+  end
+
+  def check_win?(num)
+    @win_lines.each do |line_indexes|
+      next unless line_indexes.include?(num)
+
+      win = true
+      line_indexes.each do |index|
+        win = false if @board[index] != @board[num]
+      end
+
+      next unless win
+
+      puts "You win!"
+      show_board
+      return true
+    end
+    false
+  end
+end
+
+game1 = TTT_Game.new
+game1.play_move
---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://www.theodinproject.com/lessons/ruby-tic-tac-toe
